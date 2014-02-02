8ef603f425e9ccfe12ce8b6b51ec5ccbf4a9c39d
Brendan Bates <batesbrendan@gmail.com>
2014-02-02 17:42:33 +1100

    Inline 'pass' message when running tests in a file

--- src/lt/plugins/lt_expectations.cljs
+++ src/lt/plugins/lt_expectations.cljs
@@ -4,9 +4,29 @@
 						[lt.objs.command :as cmd]
 						[lt.objs.editor	:as editor]
 						[lt.plugins.watches :as watches]
-						[lt.objs.editor.pool :as editor-pool])
+						[lt.objs.editor.pool :as editor-pool]
+						[cljs.reader :as reader])
   (:require-macros [lt.macros :refer [behavior]]))
 
+
+(defn- run-expectations
+	[namespace-name]
+	(let [exec `'(let [ns-pattern# (re-pattern (str "^" ~(str namespace-name) "$"))]
+							 (binding [expectations/summary (fn [msg#] (println "Rebound summary report >>>" msg#))
+												 expectations/fail (fn [test-name# test-meta# msg#] (println "Rebound the fail report>>>>" test-name# "," test-meta# "," msg#))]
+								 (expectations/run-all-tests ns-pattern#)
+								 (->> (symbol '~namespace-name)
+											ns-interns
+											vals
+											(filter #(:expectation (meta %)))
+											(map meta)
+											(map #(assoc % :ns (ns-name (:ns %)))))))] ;; cljs reader can't read namespace objects refs, for obvious reasons
+		(pr-str (second exec))))
+
+(defn- remove-expectations-ns
+	[namespace-name]
+	(let [exec `'(remove-ns '~namespace-name)]
+		(pr-str (second exec))))
 
 (def clj-lang (object/create :langs.clj))
 
@@ -14,19 +34,37 @@
 (defn- append-run-all-tests
 	[editor]
 	(let [curr-ns (-> @editor :info :ns)]
-		(str (watches/watched-range
+		(str (remove-expectations-ns curr-ns)
+		 		(watches/watched-range
 					editor
 					nil
 					nil (if (object/has-tag? editor :editor.cljs)
 								cljs-watch
 								clj-watch))
-				 "(run-all-tests #\"^" curr-ns "$\")")))
+				 (run-expectations curr-ns))))
 
+
+;;TODO: create macro that binds the fail and summary functions
+;; fail func will add failure details to atomic set
+;; maybe an easier way would be once tests are finished, to look at the metadata that expectations puts on the test vars?
+;; (reader/read-string "({:status [:fail \"[36m(expect 2 (+ 1 1000))\n[0m\n           expected: 2 \n                was: 1001\" 12],  :name G__11007, :expectation true, :column 1, :expectations/run true, :line 12, :file \"/Users/brendan/dev/icm/lt-expectations/test/lt_expectations/test_expects.clj\"} {:status [:success \"\" 6], :name G__10997, :expectation true, :column 1, :expectations/run true, :line 6, :file \"/Users/brendan/dev/icm/lt-expectations/test/lt_expectations/test_expects.clj\"})")
 
 (behavior ::expect-result
 					:triggers #{:editor.eval.clj.result.expect}
 					:reaction (fn [obj res]
-											(println ">>>>" res)))
+											(println ">>>>" (last (:results res)))
+											(let [expect-results (reader/read-string (:result (last (:results res))))]
+												(doseq [result expect-results]
+													(let [loc {:line (dec (:line result))
+																		 :ch 0 :start-line (dec (:line result))}]
+														(if (= :success (first (:status result)))
+															(object/raise obj :editor.result "pass" loc))))
+
+												)
+												)
+
+
+											)
 
 
 
@@ -34,6 +72,7 @@
               :desc "Expectations: Run expectations in current file"
               :exec (fn []
 											(when-let [ed (editor-pool/last-active)]
+												(object/add-behavior! ed ::expect-result) ;;FIXME: shouldn't have to do this!
 												(object/raise clj-lang :eval!
 											 {:origin ed
 												:info (assoc (@ed :info)
